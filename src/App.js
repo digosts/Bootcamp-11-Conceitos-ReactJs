7142178e2e42320dae8e1ebffa1d67e7328729b3
digosts <rsantosdesenv@gmail.com>
2020-04-14 11:36:54 -0300

    Desenvolvimento de desafio de reactjs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,87 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState([]);
+
+  const [repositories, setRepositories] = useState([]);
+
+  function getRepositories(){
+    api.get('/repositories').then(response => {
+      setRepositories(response.data); 
+    });
+  }
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const arrayTechs = techs.split(',');
+
+    const data = {
+      title,url, techs:arrayTechs
+    }
+
+    try{
+      const response = await api.post('/repositories', data);
+
+      const repositorie =  response.data;
+
+      alert('Cadastro realizado com sucesso!');
+
+      setRepositories([
+        ...repositories, repositorie
+      ]);
+
+      setTitle('');
+      setUrl('');
+      setTechs([]);
+    }catch(e){
+      alert('Erro ao tentar realizar o cadastro!');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete('/repositories/'+id+'');
+    getRepositories();
   }
+
+  useEffect(getRepositories, []);
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      <input 
+        placeholder="Title" 
+        value={title}
+        onChange={e=>setTitle(e.target.value)}/>
+      
+      <input 
+        placeholder="Url"
+        value={url}
+        onChange={e=>setUrl(e.target.value)}/>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+      <input 
+        placeholder="Techs separados por ','"
+        value={techs}
+        onChange={e=>setTechs(e.target.value)}/>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+      
+      <ul data-testid="repository-list">
+        {repositories.map(repo => 
+          <li key={repo.id}>
+            <span>
+              {repo.title}
+            </span>
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
